5dc58cb82a8ca5d9f4131fb9fed3db519dca0db7
gabrielssa <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2016-09-13 09:00:51 -0300

    classe e teste de hospede criadas

---
+++ src/componentes/Hospede.java
@@ -0,0 +1,63 @@
+package componentes;
+
+public class Hospede {
+	private String nome;
+	private String email;
+	private String dataNascimento;
+	
+	public Hospede(String nome, String email, String dataNascimento){
+		this.nome = nome;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Hospede))
+			return false;
+		Hospede other = (Hospede) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		return true;
+	}
+	
+}
