1ae2aa151540b89ba5568aff26f0395f39b5128b
ygorouriques <ygor.rodrigues@ccc.ufcg.edu.br>
2016-09-14 11:16:02 -0300

    Facade criada

---
+++ src/componentes/Facade.java
@@ -0,0 +1,32 @@
+package componentes;
+
+import componentes.Hospede;
+import execeptions.EmailInvalidoException;
+import execeptions.NomeInvalidoException;
+import componentes.Controler;
+
+public class Facade {
+
+	private Controler controller;
+	
+	public Facade(){
+		controller = new Controler();
+	}
+	
+	public String cadastraHospede(String nome, String email, String dataNascimento) throws NomeInvalidoException, EmailInvalidoException{
+		return controller.cadastraHospede(nome, email, dataNascimento);
+	}
+	public Hospede buscaHospede(String email){
+		return controller.buscaHospede(email);
+	}
+	public String getInfoHospede(String email, String atributo){
+		return controller.getInfoHospede(email, atributo);
+	}
+	public void atualizaCadastro(String id, String atributo, String valor){
+		controller.atualizaCadastro(id, atributo, valor);
+	}
+	public boolean removeHospede(String email){
+		return controller.removeHospede(email);
+	}
+	
+}
