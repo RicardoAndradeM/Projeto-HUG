1576c550e754b54b3fc882c62f9df960d8bcaa65
gabrielssa <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2016-09-18 19:31:08 -0300

    ControlerRestaurante e correcoes na estadia

---
+++ src/componentes/ControlerRestaurante.java
@@ -0,0 +1,127 @@
+package componentes;
+
+import java.util.HashMap;
+
+public class ControlerRestaurante {
+	private final String NOME = "nome";
+	private final String DESCRICAO = "descricao";
+	private final String PRECO = "preco";
+	
+	private HashMap<Prato> pratos;
+	private ArrayList<Refeicao> refeicoes;
+	
+	public ControlerRestaurante(){
+		pratos = new HashMap<Prato>();
+		refeicoes = new ArrayList<Refeicao>();
+	}
+	
+	public boolean cadastraPrato(String nome, double preco, String descricao) throws Exception{
+			if (! (buscaPrato(nome)==null)){
+				throw new Exception("prato jah esta cadastrado");
+				
+			}else{
+				Prato novoPrato = new Prato(String nome, double preco, String descricao);
+				pratos.put(nome, Prato);
+				return true;
+			}
+	}
+	
+	public boolean cadastraRefeicao(String nome, String descricao, Prato primeiroPrato, Prato segundoPrato, Prato terceiroPrato) throws Exception{
+			if (! (buscaRefeicao(nome)==null)){
+				throw new Exception("Refeicao jah esta cadastrada");
+			}else{
+				Refeicao novaRefeicao = new Refeicao(nome, descricao, primeiroPrato,segundoPrato, terceiroPrato);
+				refeicoes.add(Refeicao);
+				return true;
+			}
+	}
+	
+	public boolean cadastraRefeicao(String nome, String descricao, Prato primeiroPrato, Prato segundoPrato, Prato terceiroPrato, Prato quartoPrato){
+		if (! (buscaRefeicao(nome)==null)){
+			throw new Exception("Refeicao jah esta cadastrada");
+		}else{
+			Refeicao novaRefeicao = new Refeicao(nome, descricao, primeiroPrato,segundoPrato, terceiroPrato, quartoPrato);
+			refeicoes.add(Refeicao);
+			return true;
+		}
+	}
+	
+	public boolean atualizaPrato(String nomePrato, String atributo, String valor){
+		Prato prato = pratos.get(nomePrato);
+		
+		if (buscaPrato(nomePrato)==null){
+			throw new Exception("Prato nao existe");
+		}
+		
+		if (atributo.equals(NOME)){
+			prato.setNome(valor);
+			return true;
+		}else if (atributo.equals(DESCRICAO)){
+			prato.setDescricao(valor);
+			return true;
+		}else if (atributo.equals(PRECO)){
+			prato.setPreco(Double.parseDouble(valor));
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean atualizaRefeicao(String nomeRefeicao, String atributo, String valor){
+		if (buscaRefeicao(nomeRefeicao)==null){
+			throw new Exception("Refeicao nao existe");
+		}
+		
+		Refeicao refeicao = buscaRefeicao(nomeRefeicao);
+		
+		if (atributo.equals(NOME)){
+			refeicao.setNome(valor);
+			return true;
+		}else if (atributo.equals(DESCRICAO)){
+			refeicao.setDescricao(valor);
+			return true;
+		}
+		return false;
+	}
+	
+
+	
+	public Refeicao buscaRefeicao(String nomeRefeicao){
+		for (Refeicao refeicao : refeicoes){
+			if (refeicao.getNome.equals(nomeRefeicao)){
+				return refeicao;
+			}
+		}
+		return null;
+	}
+	
+	public boolean verificaPrato(String nomePrato){
+		if (pratos.get(nomePrato)==null){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	public boolean verificaRefeicao(String nomeRefeicao){
+		if (buscaRefeicao(nomeRefeicao)==null){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	public boolean removePrato(String nomePrato) throws Exception{
+		if (verificaPrato(nomePrato)){
+			pratos.remove(nomePrato);
+			return true;
+		}else{
+			throw new Exception("Prato nao existe");
+		}
+	}
+	
+	public boolean removeRefeicao(String nomeRefeicao) throws Exception{
+		if (verificaRefeicao(nomeRefeicao)){
+			refeicoes.remove(buscaRefeicao(nomeRefeicao));
+		}
+	}
+}
--- src/componentes/Estadia.java
+++ src/componentes/Estadia.java
@@ -29,6 +29,29 @@ public class Estadia {
 		quarto.setNumeroDeQuarto(novoQuartoID);
 	}
 	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + quantidadeDias;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Estadia))
+			return false;
+		Estadia other = (Estadia) obj;
+		if (quantidadeDias != other.quantidadeDias)
+			return false;
+		return true;
+	}
+
 	private void verificaQuantidadedeDias(int quantidadeDias) throws QuantidadedeDiasInvalidaException {
 		if (quantidadeDias<=0){
 			throw new QuantidadedeDiasInvalidaException("Quantidade de dias invalida");
