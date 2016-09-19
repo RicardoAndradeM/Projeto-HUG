80ea147c901483ac3b423190e422d3e042e70540
gabrielssa <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2016-09-19 09:48:28 -0300

    Colocando os testes para o main

--- src/componentes/Facade.java
+++ src/componentes/Facade.java
@@ -36,18 +36,4 @@ public class Facade {
 		return controller.buscaHospede(email);
 	}
 	
-	 public static void main(String[] args) {
-	        args = new String[] { 
-	        		"componentes.Facade", "testacept/testes_uc1.txt/",
-	                "testacept/testes_uc1_exception.txt/", 
-	                "testacept/testes_uc2.txt/",
-	                "testacept/testes_uc2_exception.txt/", 
-	                "testacept/testes_uc3.txt/",
-	                "testacept/testes_uc3_exception.txt/", 
-	                "testacept/testes_uc4.txt/",
-	                "testacept/testes_uc4_exception.txt/" 
-	                };
-	        EasyAccept.main(args);
-	    }
-	
 }
