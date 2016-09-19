c5ff32f6f0c57004fe74e6fb9fd6039894d18417
ygorouriques <ygor.rodrigues@ccc.ufcg.edu.br>
2016-09-19 16:26:06 -0300

    atualizacao da classe Refeicao

--- src/componentes/Refeicao.java
+++ src/componentes/Refeicao.java
@@ -75,5 +75,36 @@ public class Refeicao {
 	public void autualizaPrato(int posicaoPrato, Prato prato){
 		pratos.add(posicaoPrato, prato);
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((pratos == null) ? 0 : pratos.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Refeicao other = (Refeicao) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (pratos == null) {
+			if (other.pratos != null)
+				return false;
+		} else if (!pratos.equals(other.pratos))
+			return false;
+		return true;
+	}
+
 }
