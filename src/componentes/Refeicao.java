9b470f1e29d1b608334d92d858bc7053ad32d692
ygorouriques <ygor.rodrigues@ccc.ufcg.edu.br>
2016-09-19 14:52:17 -0300

    Classe Refeicao criada

---
+++ src/componentes/Refeicao.java
@@ -0,0 +1,71 @@
+package componentes;
+
+import java.util.ArrayList;
+
+import execeptions.NomeDeAtributoInvalidoException;
+
+public class Refeicao {
+	private String nome;
+	private String descricao;
+	private ArrayList<Prato> pratos;
+	
+	
+	
+	public Refeicao(String nome, String descricao, Prato primeiroPrato, Prato segundoPrato, Prato terceiroPrato) throws NomeDeAtributoInvalidoException {
+		if(nome == null|| nome.equals("")){
+			throw new NomeDeAtributoInvalidoException("Nome invalido");
+		}
+		if( descricao == null || descricao.equals("")){
+			throw new NomeDeAtributoInvalidoException("Descricao invalida");
+		}
+		pratos.add(primeiroPrato);
+		pratos.add(segundoPrato);
+		pratos.add(terceiroPrato);
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+	
+	public Refeicao(String nome, String descricao, Prato primeiroPrato, Prato segundoPrato, Prato terceiroPrato, Prato quartoPrato) throws NomeDeAtributoInvalidoException {
+		if(nome == null|| nome.equals("")){
+			throw new NomeDeAtributoInvalidoException("Nome invalido");
+		}
+		if( descricao == null || descricao.equals("")){
+			throw new NomeDeAtributoInvalidoException("Descricao invalida");
+		}
+		pratos.add(primeiroPrato);
+		pratos.add(segundoPrato);
+		pratos.add(terceiroPrato);
+		pratos.add(quartoPrato);
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+	
+	public double getValor(){
+		double valor = 0;
+		for (Prato prato : pratos) {
+			valor += prato.getPreco();
+		}
+		return valor - (valor*0.1);
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public void autualizaPrato(int posicaoPrato, Prato prato){
+		pratos.add(posicaoPrato, prato);
+	}
+	
+}
